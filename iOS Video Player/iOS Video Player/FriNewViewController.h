7092d8449b985b5e632eeef478916385888c86c2
M <m@Ms-MacBook-Air.att.net>
2015-06-19 12:42:44 -0700

    Friday New

---
+++ iOS Video Player/iOS Video Player/FriNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriNewViewController.h
+//  iOS Video Player
+//
+//  Created by M. on 6/19/15.
+//  Copyright (c) 2015 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriNewViewController : UIViewController
+
+@end
---
+++ iOS Video Player/iOS Video Player/FriNewViewController.m
@@ -0,0 +1,37 @@
+//
+//  FriNewViewController.m
+//  iOS Video Player
+//
+//  Created by M. on 6/19/15.
+//  Copyright (c) 2015 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "FriNewViewController.h"
+
+@interface FriNewViewController ()
+
+@end
+
+@implementation FriNewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
